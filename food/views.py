5e2d054a98a670cfaab11e8989556d8d4c29a4b8
Kevin Hsu <kevinhsu1999@gmail.com>
2025-02-22 09:21:22 +0800

    added signals and class-based views

--- food/views.py
+++ food/views.py
@@ -4,32 +4,53 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
-def index(request):
-    item_list = Item.objects.all()
-    # template = loader.get_template('food/index.html')
-    context = {
-        'item_list': item_list,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'food/index.html', context)
+# def index(request):
+#     item_list = Item.objects.all()
+#     # template = loader.get_template('food/index.html')
+#     context = {
+#         'item_list': item_list,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'food/index.html', context)
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item': item,
-    }
-    return render(request, 'food/detail.html', context)
+# def detail(request, item_id):
+#     item = Item.objects.get(pk=item_id)
+#     context = {
+#         'item': item,
+#     }
+#     return render(request, 'food/detail.html', context)
 
-def create_item(request):
-    form = ItemForm(request.POST or None)
+# def create_item(request):
+#     form = ItemForm(request.POST or None)
 
-    if form.is_valid():
-        form.save()
-        return redirect('food:index')
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food:index')
     
-    return render(request, 'food/item-form.html', {'form': form}) 
+#     return render(request, 'food/item-form.html', {'form': form}) 
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+# this is a class based view for create item 
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 def update_item(request, item_id):
     item = Item.objects.get(pk=item_id)
